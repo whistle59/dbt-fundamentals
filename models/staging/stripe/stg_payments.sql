5ef9e396aa2378791368a1ff6314f6b1805cddf7
mrooney@cybericum.com <mrooney@cybericum.com>
2022-03-12 15:44:10 +0000

    commit after completing sources yml files

---
+++ models/staging/stripe/stg_payments.sql
@@ -0,0 +1,17 @@
+ with payments as (
+
+     select
+        id as payment_id,
+        orderid as order_id,
+        paymentmethod as payment_method,
+        status,
+
+        -- amount is storesd in cents, convert it to dollars
+        amount / 100 as amount,
+        created as created_at
+    
+    from {{ source('jaffle_shop','payments') }}
+
+ )
+
+ select * from payments
